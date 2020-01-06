de010983160031ce46ea34326b5863be240d9fee
unknown <g.mameniskis1@gmail.com>
2020-01-06 15:56:42 -0500

    test needs work

--- src/test/java/com/github/curriculeon/TestPerson.java
+++ src/test/java/com/github/curriculeon/TestPerson.java
@@ -1,5 +1,35 @@
 package com.github.curriculeon;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestPerson {
+    // given
+    private void test(String expectedName) {
+        Person person = new Person();
+        person.setName(expectedName);
+    }
+    // when
+    String actualName = person.getName();
+    // then
+    Assert.assertEquals(expectedName, actualName);
+
+
+
+    @Test
+    public void test0() {
+        test("Grayson");
+    }
+
+    @Test
+    public void test1() {
+        test("Montaniz");
+    }
+
+    @Test
+    public void test2() {
+        test("Jeffy-Jeff");
+    }
+
 
 }
