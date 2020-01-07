faf9240f4199f4ac13d6414df9b99f94102013e1
unknown <g.mameniskis1@gmail.com>
2020-01-07 15:12:18 -0500

    progress on People Class

--- src/main/java/com/github/curriculeon/People.java
+++ src/main/java/com/github/curriculeon/People.java
@@ -1,8 +1,19 @@
 package com.github.curriculeon;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class People {
 
-    Person personlist = new List[];
+// <<< FIELD >>> ---------------------------------------------------
+    List<Person> personlist = new ArrayList<>();
+
+// <<< METHOD >>> --------------------------------------------------
+    public void add(Person person) {
+        personlist.add(person);
+    }
+
+    public void findById(long id) {
+        personlist.get();
+    }
 }
